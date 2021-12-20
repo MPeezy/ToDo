4dbd8bca968fcb92cf9dca39c1750330a2d2aeb8
MPeezy <marjoriepatton@rocketmortgage.com>
2021-12-20 07:36:41 -0500

    initial commit

---
+++ ToDoApp/Controllers/EmployeeController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDoApp.Models;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using Sitecore.FakeDb;
+using XAct.Library.Settings;
+
+namespace ToDoApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        EmployeeDAL dbEmployee = new EmployeeDAL();
+        ToDoDAL dbToDo = new ToDoDAL();
+
+        public IActionResult Index()
+        {
+            EmployeeTaskViewModel etvm = new EmployeeTaskViewModel();
+            return View(etvm);
+        }
+
+        public IActionResult Details(int id)
+        {
+            Employee e = dbEmployee.GetEmployee(id);
+            return View(e);
+        }
+
+        public IActionResult CreateEmployee()
+        {
+            Employee e = new Employee();
+            e.Name = "Cotton Hill";
+            e.Title = "Retired Vet";
+            e.HoursAvailible = 30;
+            dbEmployee.AddEmployee(e);
+            return RedirectToAction("Index");
+        }
+
+        //updates database with info from form
+        public IActionResult Result(Employee e)
+        {
+            dbEmployee.AddEmployee(e);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+
+        {
+            dbEmployee.DeleteEmployee(id);
+
+            return RedirectToAction("Index");
+
+        }
+
+
+
+
+        public IActionResult EmployeeForm()
+        {
+
+            return View();
+        }
+
+
+        //public IActionResult Update(int id)
+
+        //{
+        //    List<Employee> students = db.GetEmployees();
+        //    Employee e = students.Where(x => x.Id == id).ToList().First();
+        //    e.HoursAvailible--;
+        //    db.UpdateEmployee(e);
+
+        //    return RedirectToAction("Index", "Home");
+
+
+
+
+        //}
+
+
+
+
+    }
+}
---
+++ ToDoApp/Models/EmployeeDAL.cs
@@ -0,0 +1,80 @@
+﻿using Dapper;
+using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoApp.Models
+{
+    public class EmployeeDAL
+    {
+        //Get CRUD Actions working 
+        //Once we get this working in employee we can then reuse most of this code for ToDo 
+        public List<Employee> GetEmployees()
+        {
+            using(var connect = new MySqlConnection(Secret.Connection))
+            {
+                string sql = "select * from employee";
+                connect.Open();
+                List<Employee> employees = connect.Query<Employee>(sql).ToList();
+                connect.Close();
+                return employees;
+            }
+        }
+
+        public Employee GetEmployee(int id)
+        {
+            using (var connect = new MySqlConnection(Secret.Connection))
+            {
+                string sql = "select * from employee where id=" +id;
+                connect.Open();
+                Employee employee = connect.Query<Employee>(sql).ToList().First();
+                connect.Close();
+                return employee;
+            }
+        }
+
+        public void AddEmployee(Employee e)
+        {
+            using (var connect = new MySqlConnection(Secret.Connection))
+            {
+                string sql = $"insert into employee values(0, '{e.Name}', {e.HoursAvailible}, '{e.Title}')";
+                connect.Open();
+                connect.Query<Employee>(sql);
+                connect.Close();
+            }
+        }
+
+        public void DeleteEmployee(int id)
+        {
+            using (var connect = new MySqlConnection(Secret.Connection))
+            {
+                string sql = "delete from employee where id = " + id;
+                connect.Open();
+                connect.Query<Employee>(sql);
+                connect.Close();
+            }
+        }
+
+        public void UpdateEmployee(Employee e)
+
+        {
+            using (var connect = new MySqlConnection(Secret.Connection))
+            {
+                string sql = $"update employee " + $"set Name = '{e.Name}', hoursAvailible = { e.HoursAvailible}, Title = { e.Title } " +
+                  $"where id = { e.Id};";
+
+                connect.Open();
+                connect.Query<Employee>(sql);
+                connect.Close();
+
+
+
+
+            }
+        }
+
+
+    }
+}
